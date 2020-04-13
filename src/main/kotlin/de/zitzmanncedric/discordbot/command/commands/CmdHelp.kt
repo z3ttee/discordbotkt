885610b9ad58c08c1c7481fa3b70714b6b31477b
z3ttee <cedric.zitzmann@gmail.com>
2020-04-13 19:54:02 +0200

    Added help

--- src/main/kotlin/de/zitzmanncedric/discordbot/command/commands/CmdHelp.kt
+++ src/main/kotlin/de/zitzmanncedric/discordbot/command/commands/CmdHelp.kt
@@ -5,13 +5,40 @@ import de.zitzmanncedric.discordbot.command.Command
 import de.zitzmanncedric.discordbot.command.handler.CommandHandler
 import de.zitzmanncedric.discordbot.command.sender.DiscordSender
 import de.zitzmanncedric.discordbot.command.sender.Sender
+import java.awt.Color
+import java.util.function.Consumer
 
 class CmdHelp : Command("help", "", "Zeigt den Hilfecenter", Category.GENERAL) {
 
     override fun execute(sender: Sender, args: ArrayList<String>) {
 
         if(sender is DiscordSender){
+            sender.private().sendTextWitEmbed("Hilfecenter & Befehlübersicht", Consumer { embed -> run {
+                embed.setDescription("Hier ist die von dir angeforderte Übersicht über alle Befehle")
 
+                for(category in Category.values()) {
+                    val title = "${category.emoji} ${category.title}"
+                    var content = ""
+                    // TODO: Add prefix
+
+                    val commands = ArrayList<Command>(CommandHandler.commands.values.filter { it.category == category })
+                    if(commands.isEmpty()){
+                        content = "Keine Befehle für diese Kategorie gefunden."
+                    } else {
+                        for(command in commands) {
+                            val usage = when (command.usage.isEmpty() || command.usage.isBlank()) {
+                                true -> ""
+                                else -> "${command.usage} "
+                            }
+
+                            content += "` ${command.name} $usage`: ${command.description} \n"
+                        }
+                    }
+
+                    embed.addField(title, content, false)
+                }
+
+            }}).subscribe()
 
             return
         }
