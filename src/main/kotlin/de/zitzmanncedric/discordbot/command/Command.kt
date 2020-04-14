35450719e46bffe215e7bb0db7a1f4f6cb953f96
z3ttee <cedric.zitzmann@gmail.com>
2020-04-14 19:54:20 +0200

    Added usage check

--- src/main/kotlin/de/zitzmanncedric/discordbot/command/Command.kt
+++ src/main/kotlin/de/zitzmanncedric/discordbot/command/Command.kt
@@ -1,8 +1,14 @@
 package de.zitzmanncedric.discordbot.command
 
 import de.zitzmanncedric.discordbot.command.sender.Sender
+import de.zitzmanncedric.discordbot.language.Lang
 import discord4j.core.`object`.entity.Guild
+import discord4j.core.`object`.entity.Message
 
 abstract class Command(val name: String, val usage: String, val description: String, val category: Category) {
-    abstract fun execute(sender: Sender, guild: Guild, args: ArrayList<String>)
+    abstract fun execute(sender: Sender, message: Message?, guild: Guild?, args: ArrayList<String>)
+
+    fun sendUsage(sender: Sender){
+        sender.sendError(Lang.getString("error_cmd_usage").replace("%usage%", "$name $usage")).subscribe()
+    }
 }
